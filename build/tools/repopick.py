44980d4e6b79565b7e92ee01008505abb8203c9a
abun880007 <abun880007@msn.com>
2020-03-06 18:07:49 +0000

    TUB-10 Time

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2013-15 The CyanogenMod Project
-# Copyright (C) 2019 The Dirty Unicorns Project
+# Copyright (C) 2019 TUB
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
